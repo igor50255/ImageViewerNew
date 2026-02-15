8ece14418961b116b626ec8d484f60df6836e2b0
Igor Sicorskiy <idriveview@gmail.com>
2026-02-15 13:16:41 +0300

    Добавил WebView2 в проект

--- ImageViewerNew/MainWindow.xaml.cs
+++ ImageViewerNew/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ImageViewerNew;
 
@@ -19,5 +12,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Loaded += MainWindow_Loaded;
+    }
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        await Browser.EnsureCoreWebView2Async();
+
+        // 1) Маппим папку wwwroot (из Output) на виртуальный домен https://app/
+        string wwwRoot = System.IO.Path.Combine(AppContext.BaseDirectory, "wwwroot");
+        Directory.CreateDirectory(wwwRoot);
+
+        Browser.CoreWebView2.SetVirtualHostNameToFolderMapping(
+            "app",
+            wwwRoot,
+            CoreWebView2HostResourceAccessKind.Allow
+        );
+
+        // 2) Слушаем сообщения JS -> C#
+        Browser.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+
+        // 3) Открываем страницу
+        Browser.CoreWebView2.Navigate("https://app/index.html");
+    }
+
+    private async void CoreWebView2_WebMessageReceived(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+    {
+
     }
 }
